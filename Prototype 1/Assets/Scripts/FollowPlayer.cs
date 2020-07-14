bdd5545bb946e382a245adbc1d3ed45a5f20ef15
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-13 23:43:12 -0500

    Add rear view

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -7,30 +7,52 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
 
+    private Vector3 rearView = new Vector3(0, 5, 12);
     private Vector3 close = new Vector3(0, 5, -7);
     private Vector3 medium = new Vector3(0, 5, -9);
     private Vector3 far = new Vector3(0, 5, -11);
-    private Vector3[] offsets = new Vector3[3];
+    private Vector3[] offsetArray = new Vector3[3];
+    private Vector3 offset;
 
     private int offsetIndex = 0;
+
+    private bool isRearView = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        offsets[0] = close;
-        offsets[1] = medium;
-        offsets[2] = far;
+        offsetArray[0] = close;
+        offsetArray[1] = medium;
+        offsetArray[2] = far;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        // Cycle through views with C key
+        if (!isRearView)
         {
-            print(offsetIndex);
-            offsetIndex = (offsetIndex + 1) % 3;
-            print(offsetIndex);
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                offsetIndex = (offsetIndex + 1) % 3;
+            }
+            offset = offsetArray[offsetIndex];
         }
-        transform.position = player.transform.position + offsets[offsetIndex];
+
+        // While the R key is held, rear view
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isRearView = true;
+            offset = rearView;
+            transform.Rotate(Vector3.up, 180.0f, Space.World);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            isRearView = false;
+            transform.Rotate(Vector3.up, 180.0f, Space.World);
+        }
+
+        transform.position = player.transform.position + offset;
     }
 }
