339c6fd5113cc216e80facfbbc477ea4efa1959b
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-13 21:00:41 -0500

    Add propeller spin

--- Prototype 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpinPropellerX : MonoBehaviour
 {
+    private float propellerSpeed = 1000.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class SpinPropellerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward, propellerSpeed * Time.deltaTime);
     }
 }
