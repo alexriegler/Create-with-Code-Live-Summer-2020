b18eecc77260c4f84533898149af32962aa9b0bb
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-13 22:30:24 -0500

    Add auto driving car

--- Prototype 1/Assets/Scripts/AIController.cs
+++ Prototype 1/Assets/Scripts/AIController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
+    private float speed = 15.0f;
+    // private float turnSpeed = 45.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Moves the vehicle forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // Rotates the car based on horizontal input
+        // transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
