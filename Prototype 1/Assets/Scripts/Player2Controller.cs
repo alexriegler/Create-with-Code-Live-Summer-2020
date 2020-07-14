0069a7d7b675cbb5ce1b5c4082b909b7102df8c3
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-14 11:30:24 -0500

    Update controller scripts

--- Prototype 1/Assets/Scripts/Player2Controller.cs
+++ Prototype 1/Assets/Scripts/Player2Controller.cs
@@ -18,8 +18,8 @@ public class Player2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal2");
+        forwardInput = Input.GetAxis("Vertical2");
 
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -18,8 +18,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal1");
+        forwardInput = Input.GetAxis("Vertical1");
 
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
