fdf909a3593fa9e7625b39515d2466425a914e9a
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-14 13:01:00 -0500

    Use same controller for both players

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 20.0f;
     private float turnSpeed = 45.0f;
+
+    public string horizontalAxisName;
+    public string verticalAxisName;
+
     private float horizontalInput;
     private float forwardInput;
 
@@ -18,8 +22,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal1");
-        forwardInput = Input.GetAxis("Vertical1");
+        horizontalInput = Input.GetAxis(horizontalAxisName);
+        forwardInput = Input.GetAxis(verticalAxisName);
 
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
@@ -29,6 +33,6 @@ public class PlayerController : MonoBehaviour
 
     public void Celebrate()
     {
-        print("Player 1 Celebrates!");
+        print(gameObject.name + " Celebrates!");
     }
 }
