f1acba3740f3559fd4b257c9741d4424e7adc1e7
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-14 17:40:40 -0500

    Add broken wheel rotate

--- Prototype 1/Assets/Scripts/SimpleCarController.cs
+++ Prototype 1/Assets/Scripts/SimpleCarController.cs
@@ -2,11 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
 public class SimpleCarController : MonoBehaviour
 {
-    public List<AxleInfo> axleInfos; // the information about each individual axle
-    public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            // print("no wheel found");
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        print("Position: " + position);
+        print("Rotation: " + rotation);
+
+        print("Cylinder Position: " + visualWheel.transform.position);
+        print("Cylinder Rotation: " + visualWheel.transform.rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
 
     public void FixedUpdate()
     {
@@ -25,15 +60,8 @@ public class SimpleCarController : MonoBehaviour
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
             }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
     }
-}
-
-[System.Serializable]
-public class AxleInfo
-{
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    public bool motor; // is this wheel attached to motor?
-    public bool steering; // does this wheel apply steer angle?
 }
