47006effd971e049242a0fbb02d54a23c07aec6e
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-16 21:00:47 -0500

    Add methods to Vehicle.cs

--- Prototype 1/Assets/Scripts/Vehicle.cs
+++ Prototype 1/Assets/Scripts/Vehicle.cs
@@ -1,18 +1,228 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using System;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
 
 public class Vehicle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Car wheels
+    [Header("Wheel list")]
+    public List<AxleInfo> axleInfos;
+
+    [Header("RPM")]
+    public float idealRPM = 100f;
+    public float maxRPM = 1400f;
+
+    [Header("Torque, Steering, & Braking")]
+    public float maxMotorTorque = 2000f;
+    public float minMotorTorqueRatio = 0.1f;
+    // TODO: Change to vector2d?
+    public float maxSteeringAngle = 10f;
+    public float minSteeringAngle = 2f;
+    [SerializeField]
+    private float steeringAngle;
+    public float brakeTorque = 1000f;
+    public float brakeForce = 20000f;
+
+    private bool brakesApplied = false;
+
+    [Header("Anti-Roll")]
+    public float antiRollStrength = 20000.0f;
+
+    [Header("Center of Mass")]
+    public Vector3 comOffset;
+    public Rigidbody vehicle;
+
+    // Properties
+
+    // Get the maximum speed of the vehicle
+    public float MaxSpeed
     {
-        
+        get
+        {
+            WheelCollider wheel = axleInfos[0].rightWheel;
+            return wheel.radius * Mathf.PI * maxRPM * 60.0f / 1000.0f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Methods
+
+    // Finds the velocity of the vehicle based on its position & previous position
+    float GetVehicleVelocity()
     {
-        
+        Vector3 velocityVector = vehicle.velocity;
+        float velocity = velocityVector.magnitude;
+
+        // Check sign of the velocity
+        if (Vector3.Dot(Vector3.forward, velocityVector) < 0)
+        {
+            velocity = -velocity;
+        }
+
+        return velocity;
     }
+
+    // Returns the wheel velocity in km/h
+    public float GetWheelVelocity()
+    {
+        WheelCollider wheel = axleInfos[0].rightWheel;
+        return wheel.radius * Mathf.PI * wheel.rpm * 60.0f / 1000.0f;
+    }
+
+    // Returns the rotations per minute of the wheels
+    public float GetWheelRpm()
+    {
+        return axleInfos[0].rightWheel.rpm;
+    }
+
+    // Linearly interpolates the steering angle from max to min based on speed
+    void AdjustSteering()
+    {
+        steeringAngle = Mathf.Lerp(maxSteeringAngle, minSteeringAngle, GetWheelVelocity() / MaxSpeed);
+    }
+
+    // Controls the amount of torque applied to the wheels dependent upon the rpm
+    void ControlTorque(ref float motorTorque)
+    {
+        if (GetWheelRpm() < idealRPM)
+        {
+            // Apply limited torque while below ideal rpm and max torque while at ideal rpm
+            motorTorque = Mathf.Lerp(motorTorque * minMotorTorqueRatio, motorTorque, GetWheelRpm() / idealRPM);
+        }
+        else
+        {
+            // Apply max torque at ideal rpm and zero torque at max rpm
+            motorTorque = Mathf.Lerp(motorTorque, 0f, (GetWheelRpm() - idealRPM) / (maxRPM - idealRPM));
+        }
+    }
+
+    // Apply force on the wheels to prevent tipping
+    void Stabilize()
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            WheelCollider leftWheel = axleInfo.leftWheel;
+            WheelCollider rightWheel = axleInfo.rightWheel;
+            WheelHit wheelHit;
+            float leftTravel = 1.0f;
+            float rightTravel = 1.0f;
+
+            bool isGroundedLeft = leftWheel.GetGroundHit(out wheelHit);
+            if (isGroundedLeft)
+            {
+                leftTravel = (-leftWheel.transform.InverseTransformPoint(wheelHit.point).y - leftWheel.radius) / leftWheel.suspensionDistance;
+            }
+
+            bool isGroundedRight = rightWheel.GetGroundHit(out wheelHit);
+            if (isGroundedRight)
+            {
+                rightTravel = (-rightWheel.transform.InverseTransformPoint(wheelHit.point).y - rightWheel.radius) / rightWheel.suspensionDistance;
+            }
+
+            float antiRollForce = (leftTravel - rightTravel) * antiRollStrength;
+
+            if (isGroundedLeft)
+            {
+                vehicle.AddForceAtPosition(leftWheel.transform.up * -antiRollForce, leftWheel.transform.position);
+            }
+            if (isGroundedRight)
+            {
+                vehicle.AddForceAtPosition(rightWheel.transform.up * antiRollForce, rightWheel.transform.position);
+            }
+        }
+    }
+
+    // Apply torque to each wheel
+    void ApplyTorque(float motor, float steering)
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+
+    // Apply transforms to corresponding visual wheels
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    // Applies brake torque to each wheel and applies a brake force to the car
+    void ApplyBrakes()
+    {
+        bool isGrounded = false;
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            axleInfo.leftWheel.brakeTorque = brakeTorque;
+            axleInfo.rightWheel.brakeTorque = brakeTorque;
+
+            // Check if any of the wheels are on the ground
+            if (axleInfo.leftWheel.isGrounded || axleInfo.rightWheel.isGrounded)
+            {
+                isGrounded = true;
+            }
+        }
+
+        // Check if the vehicle is grounded
+        if (isGrounded)
+        {
+            // Apply force in opposite direction of travel
+            vehicle.AddForce(vehicle.velocity.normalized * -brakeForce);
+        }
+    }
+
+    // Sets brake torque to zero
+    void ReleaseBrakes()
+    {
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            axleInfo.leftWheel.brakeTorque = 0.0f;
+            axleInfo.rightWheel.brakeTorque = 0.0f;
+        }
+    }
+
+    /*
+    // Debug
+
+    // Draw center of mass gizmo
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + transform.rotation * comOffset, 0.4f);
+    }
+    */
 }
