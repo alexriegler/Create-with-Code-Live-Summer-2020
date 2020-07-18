d16dd0446bb88baf656332de8413ea40a7e475cf
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-18 09:23:11 -0500

    Change speedometer display to mph

--- Prototype 1/Assets/Scripts/Speedometer.cs
+++ Prototype 1/Assets/Scripts/Speedometer.cs
@@ -3,12 +3,14 @@ using UnityEngine.UI;
 
 public class Speedometer : MonoBehaviour
 {
+    public const float KmhToMph = 0.6213711922f;
+
     public Vehicle car;
     public Text speedText;
 
     // Update is called once per frame
     void Update()
     {
-        speedText.text = car.GetVehicleVelocity().ToString("f0") + " km/h";
+        speedText.text = (car.GetVehicleVelocity() * KmhToMph).ToString("f0") + " mph";
     }
 }
