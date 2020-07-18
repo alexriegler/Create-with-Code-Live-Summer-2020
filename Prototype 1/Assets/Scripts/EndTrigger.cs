171cae1ede21107ebffe3e387720dce329d441fe
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-18 15:40:35 -0500

    Prevent end trigger from triggering more than once

--- Prototype 1/Assets/Scripts/EndTrigger.cs
+++ Prototype 1/Assets/Scripts/EndTrigger.cs
@@ -6,10 +6,13 @@ public class EndTrigger : MonoBehaviour
 {
     public GameManager gameManager;
 
+    bool isTriggered = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isTriggered)
         {
+            isTriggered = true;
             other.GetComponent<PlayerCelebration>().Celebrate();
             gameManager.CompleteLevel(other.gameObject);
         }
