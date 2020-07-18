a6d6dac3552bb4d0267fbd9cdfda4938bc401eb6
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-17 22:51:06 -0500

    Remove debug print

--- Prototype 1/Assets/Scripts/SimpleCarController.cs
+++ Prototype 1/Assets/Scripts/SimpleCarController.cs
@@ -32,9 +32,6 @@ public class SimpleCarController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // TODO: Remove debug print
-        //print("Vw: " + car.GetWheelVelocity().ToString("f0") + "km/h   Vc: " + car.GetVehicleVelocity().ToString("f0") + "km/h   RPM: " + car.GetWheelRpm().ToString("f0"));
-
         car.AdjustSteering();
 
         GetDrivingInput();
