f43883f91d93a1219293be759c341c1da37d16f8
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-18 10:52:15 -0500

    Add alternating spawning between points

--- Prototype 1/Assets/Scripts/Respawn.cs
+++ Prototype 1/Assets/Scripts/Respawn.cs
@@ -1,25 +1,59 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
     [SerializeField]
-    private Transform respawnPoint;
+    private Transform[] respawnPointArray;
+    private bool[] respawnInUseArray;
+    private int respawnPointIndex = 0;
 
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        if (other.CompareTag("Player"))
+        respawnInUseArray = new bool[respawnPointArray.Length];
+        for (int i = 0; i < respawnPointArray.Length; i++)
         {
-            StartCoroutine(RespawnPlayer(other.gameObject));
+            respawnInUseArray[i] = false;
         }
     }
 
-    IEnumerator RespawnPlayer(GameObject player)
+    void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player"))
+        {
+            // Check if respawn point is being used
+            bool spawnInUse = respawnInUseArray[respawnPointIndex];
+            Transform respawnPoint;
+
+            if (spawnInUse)
+            {
+                IncrementRespawnIndex();
+            }
+
+            respawnPoint = respawnPointArray[respawnPointIndex];
+
+            StartCoroutine(RespawnPlayer(other.gameObject, respawnPoint));
+        }
+    }
+
+    IEnumerator RespawnPlayer(GameObject player, Transform respawnPoint)
+    {
+        respawnInUseArray[respawnPointIndex] = true;
+        IncrementRespawnIndex();
+
         player.transform.position = respawnPoint.transform.position;
         player.GetComponent<Rigidbody>().isKinematic = true;
         player.transform.eulerAngles = Vector3.forward;
+
         yield return new WaitForSeconds(1);
+
         player.GetComponent<Rigidbody>().isKinematic = false;
+        respawnInUseArray[respawnPointIndex] = false;
+    }
+
+    private void IncrementRespawnIndex()
+    {
+        respawnPointIndex = (respawnPointIndex + 1) % respawnPointArray.Length;
     }
 }
