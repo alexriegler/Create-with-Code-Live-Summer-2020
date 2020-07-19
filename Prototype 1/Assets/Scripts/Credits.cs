63c79302d9e1a8cb176aa20fca1fdd6815323c09
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-18 22:15:37 -0500

    Add play again button

--- Prototype 1/Assets/Scripts/Credits.cs
+++ Prototype 1/Assets/Scripts/Credits.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
     public void Quit()
     {
         Debug.Log("QUIT");
