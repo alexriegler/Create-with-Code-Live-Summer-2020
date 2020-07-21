694833a403b05f578a5810f0d028dc6f973169fd
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-21 17:29:59 -0500

    Rename variable

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -29,10 +29,10 @@ public class SpawnManager : MonoBehaviour
             animalIndex = Random.Range(0, animals.Length);
 
             Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
-            float z = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f, viewPos.z)).z;
+            float screenTop = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f, viewPos.z)).z;
             screenOffset = animals[animalIndex].transform.localScale.z;
 
-            Instantiate(animals[animalIndex], new Vector3(0f, 0f, z + screenOffset), animals[animalIndex].transform.rotation);
+            Instantiate(animals[animalIndex], new Vector3(0f, 0f, screenTop + screenOffset), animals[animalIndex].transform.rotation);
         }
     }
 }
