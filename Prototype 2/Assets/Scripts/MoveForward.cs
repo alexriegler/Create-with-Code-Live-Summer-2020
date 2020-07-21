dd9097123e10701bda090f09985d9fadc605107c
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-21 11:23:44 -0500

    Move all food fly forwards in world space

--- Prototype 2/Assets/Scripts/MoveForward.cs
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
+
+    [SerializeField]
+    private float speed = 40.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
     }
 }
