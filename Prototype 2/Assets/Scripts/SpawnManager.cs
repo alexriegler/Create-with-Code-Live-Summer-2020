f0636a39a0634e4a6806c564e1664821a52eacf8
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-21 17:14:09 -0500

    Spawn animal on S press

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,12 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject[] animals;
 
+    [SerializeField]
+    private float screenOffset;
+
+    [SerializeField]
+    private KeyCode spawnKey = KeyCode.S;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +22,13 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(spawnKey))
+        {
+            Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+            float z = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f, viewPos.z)).z;
+            screenOffset = animals[0].transform.localScale.z;
+
+            Instantiate(animals[0], new Vector3(0f, 0f, z + screenOffset), animals[0].transform.rotation);
+        }
     }
 }
