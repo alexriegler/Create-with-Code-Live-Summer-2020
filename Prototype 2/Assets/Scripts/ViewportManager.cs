eba020c60500e312b3b2d729e2fcaacbdd7f5398
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-21 18:52:03 -0500

    Add properties to viewport manager

--- Prototype 2/Assets/Scripts/ViewportManager.cs
+++ Prototype 2/Assets/Scripts/ViewportManager.cs
@@ -8,15 +8,52 @@ public class ViewportManager : MonoBehaviour
     [SerializeField]
     private Camera gameCamera;
 
+    [Header("Viewport")]
+    [SerializeField]
+    private float viewportPadding = 0.1f;
+    [SerializeField]
+    private float leftViewportBorder;
+    [SerializeField]
+    private float rightViewportBorder;
+
     // Start is called before the first frame update
     void Start()
     {
         gameCamera = Camera.main;
+        leftViewportBorder = 0f + viewportPadding;
+        rightViewportBorder = 1f - viewportPadding;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Get the amount of padding inside the viewport
+    public float ViewportPadding
+    {
+        get
+        {
+            return viewportPadding;
+        }
+    }
+
+    // Get the x value of the left border location in viewport space
+    public float LeftViewportBorder
+    {
+        get
+        {
+            return leftViewportBorder;
+        }
+    }
+
+    // Get the x value of the right border location in viewport space
+    public float RightViewportBorder
+    {
+        get
+        {
+            return rightViewportBorder;
+        }
     }
 }
