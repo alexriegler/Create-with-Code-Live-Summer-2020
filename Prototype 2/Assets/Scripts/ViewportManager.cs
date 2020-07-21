595dbb77fe210ce586cdd0f7e55db3aebc740af7
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-21 18:43:27 -0500

    Initialize camera

--- Prototype 2/Assets/Scripts/ViewportManager.cs
+++ Prototype 2/Assets/Scripts/ViewportManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ViewportManager : MonoBehaviour
 {
+    [Header("Camera")]
+    [SerializeField]
+    private Camera gameCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameCamera = Camera.main;
     }
 
     // Update is called once per frame
