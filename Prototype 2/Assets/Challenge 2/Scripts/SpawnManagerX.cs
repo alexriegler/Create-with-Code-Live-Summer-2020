0c15f8a5e2b841ae51d81e61fea84146cced7590
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-22 18:21:33 -0500

    Add random spawn interval

--- Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -12,6 +12,8 @@ public class SpawnManagerX : MonoBehaviour
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
+    private float spawnIntervalMin = 3.0f;
+    private float spawnIntervalMax = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +24,13 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        // Random spawn interval
+        spawnInterval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        // Get random ball index
         int randomBallIndex = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
