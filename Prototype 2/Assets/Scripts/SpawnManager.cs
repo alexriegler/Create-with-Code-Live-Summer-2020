12f71b3c4a15ce003c0ea642e557a58227df7aed
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-21 22:21:39 -0500

    Spawn animals within screen borders

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
-    private ViewportManager viewportManager;
+    private ViewportManager vpManager;
 
     [SerializeField]
     private GameObject[] animals;
@@ -19,14 +19,12 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private float screenOffset;
 
-    private float spawnRangeX = 20f;
-
     [SerializeField]
     private KeyCode spawnKey = KeyCode.S;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {        
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
@@ -43,7 +41,8 @@ public class SpawnManager : MonoBehaviour
         Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
         float screenTop = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f, viewPos.z)).z;
         screenOffset = animals[animalIndex].transform.localScale.z;
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0f, screenTop + screenOffset);
+        float randomXPos = Random.Range(vpManager.WLeftBorderX, vpManager.WRightBorderX);
+        Vector3 spawnPos = new Vector3(randomXPos, 0f, screenTop + screenOffset);
 
         Instantiate(animals[animalIndex], spawnPos, animals[animalIndex].transform.rotation);
     }
