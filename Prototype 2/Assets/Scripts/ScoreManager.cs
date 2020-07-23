8a7c5d81a31b51b40041c5c0667dd3f7ec4d3886
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 14:14:36 -0500

    Add feed points method

--- Prototype 2/Assets/Scripts/ScoreManager.cs
+++ Prototype 2/Assets/Scripts/ScoreManager.cs
@@ -4,6 +4,8 @@ public class ScoreManager : MonoBehaviour
 {
     public int Score { get; private set; } = 0;
 
+    private int feedPoints = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,7 @@ public class ScoreManager : MonoBehaviour
     {
         
     }
+
+    // Increases the score by feed points
+    void AddFeedPoints() => Score += feedPoints;
 }
