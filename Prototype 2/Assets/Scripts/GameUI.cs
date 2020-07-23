817a7bb522f89f554903cf4cbe179a7a4dfe59e9
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 17:51:18 -0500

    Add player and score manager variables

--- Prototype 2/Assets/Scripts/GameUI.cs
+++ Prototype 2/Assets/Scripts/GameUI.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
+    [SerializeField]
+    private Player player;
+    [SerializeField]
+    private ScoreManager scoreManager;
+
     // Start is called before the first frame update
     void Start()
     {
