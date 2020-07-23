d90a9e141ecaf72c928c7fa7dd1d13b79b881aa0
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 14:32:23 -0500

    Subscribe hit penalty to player hit

--- Prototype 2/Assets/Scripts/ScoreManager.cs
+++ Prototype 2/Assets/Scripts/ScoreManager.cs
@@ -14,7 +14,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player.OnPlayerHit += SubtractHitPenalty;
     }
 
     // Update is called once per frame
