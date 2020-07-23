ea30bbe710decfeda3e5e218ab4738d80d3f3771
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 15:22:04 -0500

    Add feed to feedable

--- Prototype 2/Assets/Scripts/IFeedable.cs
+++ Prototype 2/Assets/Scripts/IFeedable.cs
@@ -1,18 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class IFeedable : MonoBehaviour
+public interface IFeedable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Feed(int feedPower, RaycastHit hit);
 }
