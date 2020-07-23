4ea4adb73c626e499caf458707cb49cba1ae90a7
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 12:19:28 -0500

    Detect animal collisions

--- Prototype 2/Assets/Scripts/PlayerCollision.cs
+++ Prototype 2/Assets/Scripts/PlayerCollision.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            print("Game Over");
+        }
     }
 }
