d1ecc4e5347ff8a994e8d0ebeddc770f8344ef6e
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 15:18:42 -0500

    Fill in projectile script

--- Prototype 2/Assets/Scripts/Projectile.cs
+++ Prototype 2/Assets/Scripts/Projectile.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
+    public float Speed { get; set; } = 10f;
+    public float Damage { get; set; } = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,24 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float moveDistance = Speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
         
+        if (Physics.Raycast(ray, out RaycastHit hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        GameObject hitObject = hit.collider.gameObject;
+        Destroy(gameObject);
     }
 }
