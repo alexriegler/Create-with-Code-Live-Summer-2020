ad1605681f03f1890a246b23c17e76bf5836de1c
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 15:42:25 -0500

    Add functions to living entity

--- Prototype 2/Assets/Scripts/LivingEntity.cs
+++ Prototype 2/Assets/Scripts/LivingEntity.cs
@@ -2,5 +2,27 @@
 
 public class LivingEntity : MonoBehaviour, IDamageable
 {
-    
+    public int startingHealth;
+    protected int health;
+    protected bool dead;
+
+    private void Start()
+    {
+        health = startingHealth;
+    }
+
+    public void TakeHit(int damage, RaycastHit hit)
+    {
+        health -= damage;
+
+        if (health <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        dead = true;
+    }
 }
