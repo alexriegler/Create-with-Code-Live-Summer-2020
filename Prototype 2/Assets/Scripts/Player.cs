b8c36a77e1c0f29e80f354f99810a1b1ea904dc8
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 13:31:02 -0500

    Add lives to player

--- Prototype 2/Assets/Scripts/Player.cs
+++ Prototype 2/Assets/Scripts/Player.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
