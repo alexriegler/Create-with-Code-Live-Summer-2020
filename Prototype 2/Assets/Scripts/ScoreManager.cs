64efa6dca45a5770277166e892fecad96c22eb43
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 13:56:13 -0500

    Add score to score manager

--- Prototype 2/Assets/Scripts/ScoreManager.cs
+++ Prototype 2/Assets/Scripts/ScoreManager.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    private int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
