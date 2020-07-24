ec8857d5ff118af59ac0516d20b63dde5cb3018e
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-24 17:33:07 -0500

    Add get difficulty percent method

--- Prototype 2/Assets/Scripts/Difficulty.cs
+++ Prototype 2/Assets/Scripts/Difficulty.cs
@@ -2,5 +2,16 @@
 
 public static class Difficulty
 {
-    
+    [SerializeField]
+    private static float secondsToMaxDifficulty = 60;
+
+    // Returns the current difficulty percent
+    /// <summary>
+    /// Calculates the difficulty percent, which is between 0 and 1.
+    /// </summary>
+    /// <returns>The difficulty percent.</returns>
+    public static float GetDifficultyPercent()
+    {
+        return Mathf.Clamp01(Time.time / secondsToMaxDifficulty);
+    }
 }
