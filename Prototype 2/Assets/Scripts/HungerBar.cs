12e08a54fb30bb8d3ba3763b4b07bd51d0eb97b9
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-24 15:41:09 -0500

    Get animal from hunger bar

--- Prototype 2/Assets/Scripts/HungerBar.cs
+++ Prototype 2/Assets/Scripts/HungerBar.cs
@@ -2,10 +2,12 @@
 
 public class HungerBar : MonoBehaviour
 {
+    private Animal animal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animal = GetComponentInParent<Animal>();
     }
 
     // Update is called once per frame
