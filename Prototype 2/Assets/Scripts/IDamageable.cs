e7ac72c18cd3f4dc7342b80e6d4e57b65e5ddc58
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 19:16:40 -0500

    Clean up living entity

--- Prototype 2/Assets/Scripts/IDamageable.cs
+++ Prototype 2/Assets/Scripts/IDamageable.cs
@@ -3,6 +3,5 @@
 public interface IDamageable
 {
     // Object takes damage from hit equal to damage
-    void TakeHit(int damage, RaycastHit hit);
     void TakeHit(int damage);
 }
--- Prototype 2/Assets/Scripts/LivingEntity.cs
+++ Prototype 2/Assets/Scripts/LivingEntity.cs
@@ -6,21 +6,17 @@ public class LivingEntity : MonoBehaviour, IDamageable
     public int Health { get; protected set; }
     protected bool dead;
 
+    // Initializes the living entity's health to starting health
     protected virtual void Start()
     {
         Health = startingHealth;
     }
 
-    public virtual void TakeHit(int damage, RaycastHit hit)
-    {
-        Health -= damage;
-
-        if (Health <= 0 && !dead)
-        {
-            Die();
-        }
-    }
-
+    // Takes health away from the living entity equal to damage
+    /// <summary>
+    /// Deals damage to the health of the living entity.
+    /// </summary>
+    /// <param name="damage">The amount of health to take away from the living entity.</param>
     public virtual void TakeHit(int damage)
     {
         Health -= damage;
@@ -31,6 +27,10 @@ public class LivingEntity : MonoBehaviour, IDamageable
         }
     }
 
+    // Sets dead to true and destroys the living entity
+    /// <summary>
+    /// Destroys the living entity.
+    /// </summary>
     protected virtual void Die()
     {
         dead = true;
