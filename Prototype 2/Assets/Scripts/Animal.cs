c1a6d142e6501805bf6eb4fb3073126531b9ef09
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 22:08:15 -0500

    Move movement to animal script

--- Prototype 2/Assets/Scripts/Animal.cs
+++ Prototype 2/Assets/Scripts/Animal.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class Animal : LivingEntity
 {
+    [SerializeField]
+    private float speed = 40.0f;
+
     public event Action OnFeed;
     public event Action OnFullFeed;
+
+    // Moves the animals forward
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+    }
 
     // Called when the animal is fed
     public override void TakeHit(int damage)
