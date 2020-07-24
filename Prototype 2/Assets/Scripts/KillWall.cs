23c5f90f3e6f82da47e627b30fadb31440682af5
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-23 21:43:02 -0500

    Add trigger method to kill wall

--- Prototype 2/Assets/Scripts/KillWall.cs
+++ Prototype 2/Assets/Scripts/KillWall.cs
@@ -2,5 +2,19 @@
 
 public class KillWall : MonoBehaviour
 {
+    [SerializeField]
+    private Player player;
     
+    private bool damagePlayer = false;
+    private int playerDamage = 1;
+
+    // Destroys any object that triggers the collider and deals damage to the player if it is an animal and damagePlayer is true
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && damagePlayer)
+        {
+            player.TakeHit(playerDamage);
+        }
+        Destroy(other.gameObject);
+    }
 }
