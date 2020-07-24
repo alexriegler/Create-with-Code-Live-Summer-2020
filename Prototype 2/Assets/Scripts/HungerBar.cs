5a67ef44fc4a4dbebfbd0b39bdd0f1b3fd6bb0d3
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-24 15:50:36 -0500

    Update hunger bar on moose

--- Prototype 2/Assets/Scripts/HungerBar.cs
+++ Prototype 2/Assets/Scripts/HungerBar.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HungerBar : MonoBehaviour
 {
+    private Slider hungerBar;
     private Animal animal;
 
     // Start is called before the first frame update
     void Start()
     {
+        hungerBar = GetComponent<Slider>();
         animal = GetComponentInParent<Animal>();
+
+        hungerBar.maxValue = animal.startingHealth;
+        hungerBar.value = animal.startingHealth;
+
+        animal.OnFeed += () => hungerBar.value--;
     }
 
     // Update is called once per frame
