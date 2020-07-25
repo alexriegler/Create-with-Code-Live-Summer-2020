eec63455b8c3a5e2ea82f8e7033abf78a276865d
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-24 21:19:09 -0500

    Add hearts to player health

--- Prototype 2/Assets/Scripts/PlayerHealth.cs
+++ Prototype 2/Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public Toggle[] hearts;
+
+    private Stack heartStack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        heartStack = new Stack();
+
+        foreach (Toggle heart in hearts)
+        {
+            heartStack.Push(heart);
+        }
     }
 
     // Update is called once per frame
