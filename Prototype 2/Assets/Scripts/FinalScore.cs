5766c598db9155886cfddad0df90d828e1741fb7
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-24 21:49:12 -0500

    Set final score text

--- Prototype 2/Assets/Scripts/FinalScore.cs
+++ Prototype 2/Assets/Scripts/FinalScore.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class FinalScore : MonoBehaviour
 {
+    public ScoreManager scoreManager;
+
+    private Text finalScoreText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        finalScoreText = gameObject.GetComponent<Text>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        finalScoreText.text = scoreManager.Score.ToString();
     }
 }
