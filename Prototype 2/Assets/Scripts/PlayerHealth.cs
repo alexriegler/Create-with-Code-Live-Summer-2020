2f4bb76a4a04b650ee420782b3e8360fa8c6bf94
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-24 21:27:17 -0500

    Update health on player taking damage

--- Prototype 2/Assets/Scripts/PlayerHealth.cs
+++ Prototype 2/Assets/Scripts/PlayerHealth.cs
@@ -1,27 +1,34 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public Player player;
     public Toggle[] hearts;
 
-    private Stack heartStack;
+    private Stack<Toggle> heartStack;
 
     // Start is called before the first frame update
     void Start()
     {
-        heartStack = new Stack();
+        heartStack = new Stack<Toggle>(hearts.Length);
 
         foreach (Toggle heart in hearts)
         {
             heartStack.Push(heart);
         }
+
+        player.OnPlayerHit += UpdateHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Updates the health visual
+    void UpdateHealth()
     {
-        
+        if (heartStack.Peek() != null)
+        {
+            Toggle heart = heartStack.Pop();
+            heart.isOn = false;
+        }
     }
 }
