9f3617b7657c96202baad3f410bd0addf07a585d
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-25 09:42:26 -0500

    Add animal reference to game start

--- Prototype 2/Assets/Scripts/StartGame.cs
+++ Prototype 2/Assets/Scripts/StartGame.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
+    private Animal animal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animal = gameObject.GetComponent<Animal>();
     }
 
     // Update is called once per frame
