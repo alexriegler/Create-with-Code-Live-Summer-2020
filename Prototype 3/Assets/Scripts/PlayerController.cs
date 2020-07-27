29cb7880e812339fd3c2bde1eb9674ae2b576b49
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-27 16:06:35 -0500

    Add ground check

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpForce = 10;
     [SerializeField] float gravityModifier = 1;
 
-
     private Rigidbody playerRb;
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(jumpButton))
+        if (Input.GetButtonDown(jumpButton) && isGrounded)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
+
+    void OnCollisionEnter(Collision collision) => isGrounded = true;
 }
