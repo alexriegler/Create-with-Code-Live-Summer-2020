24996a6a6cb0609bf3a1a47d3580541296d293cf
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-27 17:14:43 -0500

    Add invoke repeating obstacles

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,26 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
 
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
     private Vector3 spawnPos = new Vector3(25, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Spawns an obstacle
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
