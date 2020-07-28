4e00a32c7b8982501dd122216ca166a90a3a76eb
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-28 16:39:57 -0500

    Stop obstacle spawning on gameOver

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,12 +6,13 @@ public class SpawnManager : MonoBehaviour
 
     private float startDelay = 2;
     private float repeatRate = 2;
-
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
     }
 
@@ -24,6 +25,9 @@ public class SpawnManager : MonoBehaviour
     // Spawns an obstacle
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
