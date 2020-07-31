87938d9132a597fc5305760d2144386c7b8ade50
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-31 16:29:21 -0500

    Add game start event invoke

--- Prototype 3/Assets/Scripts/GameManager.cs
+++ Prototype 3/Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
     private bool gameStarted = false;
     private bool gameOver = false;
 
+    // TODO: Do I need these?
+    public event Action OnGameStart;
     public event Action OnGameOver;
 
     // Start is called before the first frame update
@@ -20,6 +22,13 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // Starts the game
+    void StartGame()
+    {
+        print("Game Start");
+        OnGameStart?.Invoke();
     }
 
     // Ends the game
