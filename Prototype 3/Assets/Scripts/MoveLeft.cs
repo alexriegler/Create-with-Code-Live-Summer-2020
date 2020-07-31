e31ca98fa27041c5e88fb90286b1ab67b9f2131b
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-31 15:03:53 -0500

    Change boolean

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -16,7 +16,7 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false)
+        if (!playerControllerScript.gameOver)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
