9f86b84a5abd8f87475ba947621e39c7cdb7d85a
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-31 16:26:24 -0500

    Add game over and start bools

--- Prototype 3/Assets/Scripts/GameManager.cs
+++ Prototype 3/Assets/Scripts/GameManager.cs
@@ -1,16 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private PlayerController player;
+    private bool gameStarted = false;
+    private bool gameOver = false;
+
+    public event Action OnGameOver;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerController>();
+        player.OnPlayerDeath += EndGame;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Ends the game
+    void EndGame()
+    {
+        print("Game Over");
+        gameOver = true;
+        OnGameOver?.Invoke();
     }
 }
