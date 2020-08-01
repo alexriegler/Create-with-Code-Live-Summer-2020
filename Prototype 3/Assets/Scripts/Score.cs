dcd13c1c7f4de0699f88c27065f08230fe67fd8a
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-31 21:41:27 -0500

    Add double points while dashing

--- Prototype 3/Assets/Scripts/Score.cs
+++ Prototype 3/Assets/Scripts/Score.cs
@@ -3,14 +3,26 @@
 public class Score : MonoBehaviour
 {
     public int PlayerScore { get; private set; }
-    
-    private float distanceTraveled;
+
     private ScrollManager scrollManager;
+    private PlayerController player;
+    private float distanceTraveled;
+    private float scoreMultiplier;
+    private readonly float defaultMultiplier = 1;
+    private readonly float doubleMultiplier = 2;
 
     // Gets required object
     void Start()
     {
+        scoreMultiplier = defaultMultiplier;
+        
         scrollManager = FindObjectOfType<ScrollManager>();
+        player = FindObjectOfType<PlayerController>();
+
+        // Sets score multiplier to double points when player is dashing
+        player.OnPlayerStartDash += () => scoreMultiplier = doubleMultiplier;
+        // Sets score multiplier to normal points when player is finished dashing
+        player.OnPlayerEndDash += () => scoreMultiplier = defaultMultiplier;
     }
 
     // Update is called once per frame
@@ -22,7 +34,7 @@ public class Score : MonoBehaviour
     // Updates the score
     void UpdateScore()
     {
-        distanceTraveled += scrollManager.ScrollSpeed * Time.deltaTime;
+        distanceTraveled += scrollManager.ScrollSpeed * scoreMultiplier * Time.deltaTime;
         PlayerScore = Mathf.RoundToInt(distanceTraveled);
         print(PlayerScore);
     }
