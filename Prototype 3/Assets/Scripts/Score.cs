fe30d2dc7a60bae083f03ee10e8d40c7a5980aa0
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-31 21:29:57 -0500

    Add scoring

--- Prototype 3/Assets/Scripts/Score.cs
+++ Prototype 3/Assets/Scripts/Score.cs
@@ -3,16 +3,21 @@
 public class Score : MonoBehaviour
 {
     public int PlayerScore { get; private set; }
+    private float distanceTraveled;
+
+    private ScrollManager scrollManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scrollManager = FindObjectOfType<ScrollManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceTraveled += scrollManager.ScrollSpeed * Time.deltaTime;
+        PlayerScore = Mathf.RoundToInt(distanceTraveled);
+        print(PlayerScore);
     }
 }
