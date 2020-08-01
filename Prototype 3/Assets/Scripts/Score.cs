2bc18ce5e4144ea0afacd0096697d30b535da658
Alex Riegler <alexander.s.riegler@gmail.com>
2020-07-31 21:20:58 -0500

    Add score script

---
+++ Prototype 3/Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int PlayerScore { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
