580ff92f1cac2812457217e6ea8d09f92d70899d
Alex Riegler <alexander.s.riegler@gmail.com>
2020-08-03 15:12:39 -0500

    Add scroll speed curve

--- Prototype 3/Assets/Scripts/ScrollManager.cs
+++ Prototype 3/Assets/Scripts/ScrollManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ScrollManager : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class ScrollManager : MonoBehaviour
     private PlayerController player;
     private GameManager gm;
     private float defaultScrollSpeed = 20;
+    private float scrollAcceleration = 20;
 
     void Start()
     {
@@ -16,8 +18,21 @@ public class ScrollManager : MonoBehaviour
         player.OnPlayerEndDash += SetScrollSpeed;
 
         gm = FindObjectOfType<GameManager>();
-        gm.OnGameStart += SetScrollSpeed;
+        gm.OnGameStart += () => StartCoroutine(StartScroll());
         gm.OnGameOver += StopScrolling;
+    }
+
+    // Starts the scroll gradually, following a curve
+    private IEnumerator StartScroll()
+    {
+        float x = 0;
+        while (ScrollSpeed < defaultScrollSpeed)
+        {
+            // cx^3 curve, c is a constant
+            ScrollSpeed = Mathf.Clamp(scrollAcceleration * Mathf.Pow(x, 3), 0, defaultScrollSpeed);
+            x += Time.deltaTime;
+            yield return null;
+        }
     }
 
     // Sets the scroll speed to the default scroll speed
